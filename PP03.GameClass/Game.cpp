2c153bb62842b0b28525b5724f5faa308ff2ba68
Jung <20142355@vision.hoseo.edu>
2018-09-13 17:42:32 +0900

    pp04

--- PP03.GameClass/Game.cpp
+++ PP03.GameClass/Game.cpp
@@ -3,15 +3,26 @@
 bool Game::init(const char*title, int xpos, int ypos, int width, int height, bool fullscreen)
 {
 	m_pWindow = SDL_CreateWindow(title, xpos, ypos, width, height, fullscreen);
-	m_bRunning = true;
+	m_pRenderer = SDL_CreateRenderer(m_pWindow, -1, 0);
+	SDL_Surface*pTempSurface = SDL_LoadBMP("assets/NAO.bmp");
+	m_pTexture = SDL_CreateTextureFromSurface(m_pRenderer, pTempSurface);
+	SDL_FreeSurface(pTempSurface);
+	SDL_QueryTexture(m_pTexture, NULL, NULL, &m_sourceRectangle.w,&m_sourceRectangle.h);
+	SDL_RenderPresent(m_pRenderer);
 	return true;
 }
 
 void Game::render() {
+	m_destinationRectangle.x = m_sourceRectangle.x = 0;
+	m_destinationRectangle.y = m_sourceRectangle.y = 0;
+	m_destinationRectangle.w = m_sourceRectangle.w;
+	m_destinationRectangle.h = m_sourceRectangle.h;
 	SDL_RenderClear(m_pRenderer);
+	SDL_RenderCopy(m_pRenderer, m_pTexture, &m_sourceRectangle, &m_destinationRectangle);
 	SDL_RenderPresent(m_pRenderer);
 }
-
+void Game::update() {
+}
 void Game::clean() {
 	std::cout << "cleaning game\n";
 	SDL_DestroyWindow(m_pWindow);
--- PP03.GameClass/Game.h
+++ PP03.GameClass/Game.h
@@ -7,12 +7,15 @@ public:
 	~Game(){}
 	bool init(const char*title, int xpos, int ypos, int width, int height, bool fullscreen);
 	void render();
-	void update() {};
+	void update();
 	void clean();
 	void handleEvents();
 	bool running(){ return m_bRunning; }
 private:
-	SDL_Window * m_pWindow;
-	SDL_Renderer* m_pRenderer = SDL_CreateRenderer(m_pWindow, -1, 0);;
-	bool m_bRunning;
+	SDL_Window * m_pWindow= 0 ;
+	SDL_Renderer* m_pRenderer= 0;
+	bool m_bRunning=true;
+	SDL_Texture*m_pTexture;
+	SDL_Rect m_sourceRectangle;
+	SDL_Rect m_destinationRectangle;
 };
