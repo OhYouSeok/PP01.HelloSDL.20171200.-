18db2fb6fa662aa2367ff1b5aa732825b7af0306
Jung <20142355@vision.hoseo.edu>
2018-09-20 18:02:56 +0900

    06

---
+++ pp06/Game.h
@@ -0,0 +1,21 @@
+#include <SDL.h>
+#include <iostream>
+class Game
+{
+public:
+	Game() {	}
+	~Game() {}
+	bool init(const char*title, int xpos, int ypos, int width, int height, bool fullscreen);
+	void render();
+	void update();
+	void clean();
+	void handleEvents();
+	bool running() { return m_bRunning; }
+private:
+	SDL_Window * m_pWindow = 0;
+	SDL_Renderer* m_pRenderer = 0;
+	bool m_bRunning = true;
+	SDL_Texture* m_pTexture;
+	SDL_Rect m_sourceRectangle;
+	SDL_Rect m_destinationRectangle;
+};
