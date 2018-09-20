e26b063e56a8b997f209e62f3712de4b4ab16499
Jung <20142355@vision.hoseo.edu>
2018-09-20 17:22:28 +0900

    pp05

---
+++ PP05.AnimatingSprite/main.cpp
@@ -0,0 +1,16 @@
+#include "Game.h"
+#include "SDL.h"
+
+Game * g_game = 0;
+int main(int argc, char* argv[]) {
+	g_game = new Game();
+	g_game->init("PP05.AnimatingSprite", 100, 100, 1024, 720, false);
+	while (g_game->running()) {
+		g_game->handleEvents();
+		g_game->update();
+		g_game->render();
+	}
+
+	g_game->clean();
+	return 0;
+}
