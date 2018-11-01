68aba01615e1b090b4747e0c8a6aed930edb9303
id40792604 <id40792604@gmail.com>
2018-11-01 17:44:17 +0900

    PP13

--- PP13/SDLGameObject.h
+++ PP13/SDLGameObject.h
@@ -1,17 +1,19 @@
 #pragma once
 #include "GameObject.h"
+#include "Vector2D.h"
 
 class SDLGameObject : public GameObject
 {
 public:
+	Vector2D m_position;
+	Vector2D m_velocity;
+	Vector2D m_acceleration;
 	SDLGameObject(const LoaderParams* pParams);
 	virtual void draw();
-	virtual void update() =0;
-	virtual void clean() =0;
-	
+	virtual void update() = 0;
+	virtual void clean() = 0;
+
 protected:
-	int m_x;
-	int m_y;
 	int m_width;
 	int m_height;
 	int m_currentRow;
